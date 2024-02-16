1268b4406a7826bd21ce0f5a8f191d4e51b1754b
William Ryuputra <williamryuputra@yahoo.co.id>
2024-02-16 15:21:52 -0500

    Adds tsconfig.json, @types/express, @types/node

--- app.ts
+++ app.ts
@@ -1,8 +1,28 @@
+// dotenv
+import { config } from "dotenv"
+
 // Import express
-const express = require('express')
+import express from 'express'
 const app = express()
 
 // import mongoose
-const mongoose = require('mongoose')
+import mongoose from "mongoose"
+
+// port
+const port = 3000
+
+// Routes
+app.get('/',(req,res)=>{
+    res.send("test")
+})
+
+app.get('/categories',(req,res)=>{
+    res.send("categories")
+})
+
+
+app.listen(port)
+
+
 
 
