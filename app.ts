5adf4c92ccdb5c7c4bfce81b0b7d187bb4e5e2ff
William Ryuputra <williamryuputra@yahoo.co.id>
2024-02-17 10:12:26 -0500

    Connects to marvel-legends database, adds Mongoose actionFigure model

--- app.ts
+++ app.ts
@@ -1,24 +1,48 @@
 // dotenv
-import { config } from "dotenv"
+import 'dotenv/config'
+
+// Action figure schema
+import actionFigureModel from './models/actionFigure'
+// const actionFigureModel = require('./models/actionFigure')
 
 // Import express
 import express from 'express'
 const app = express()
 
 // import mongoose
-import mongoose from "mongoose"
+import {connect} from 'mongoose'
 
 // port
 const port = 3000
 
-// Routes
-app.get('/',(req,res)=>{
-    res.send("test")
+// Middlewares
+// Converts payload to json
+app.use(express.json())
+
+// Querying
+app.get('/', async(req,res)=>{
+    try{
+        const figures = await actionFigureModel.find({name:"Iron Man"})
+        res.json(figures)
+        console.log(figures)
+    }catch(err){
+        res.status(500).json({error:'Failed'})
+    }
 })
 
-app.get('/categories',(req,res)=>{
-    res.send("categories")
-})
+// Connect to db
+run()
+// connect(process.env.DB_CONNECT!).then(()=>console.log("Connected to db")).catch(error => console.log(error));
+ async function run(){
+    try{
+        await connect(process.env.DB_CONNECT!)
+        console.log("connected")
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
 
 
 app.listen(port)
---
+++ models/actionFigure.ts
@@ -0,0 +1,28 @@
+import {Schema, model} from "mongoose"
+
+// Action figure interface
+interface IActionFigure {
+    name:string;
+    character:string[];
+    series:string;
+    year:string;
+    manufacturer:string;
+    variant?:string;
+}
+
+// Action figure model
+const actionFigureSchema =  new Schema<IActionFigure>(
+    {
+        name:{type:String, required:true},
+        character:{type:[String], required:true},
+        series:{type:String, required:true},
+        year:{type:String, required:true},
+        manufacturer:{type:String, required:true},
+        variant:{type:String}
+    }
+)
+
+const actionFigureModel = model<IActionFigure>('actionfigures',actionFigureSchema)
+export default actionFigureModel
+
+
