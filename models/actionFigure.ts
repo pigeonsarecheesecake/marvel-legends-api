eed4f37573f946c4eb0a7148c04e3859d7b2b7fa
William Ryuputra <williamryuputra@yahoo.co.id>
2024-02-29 12:30:32 -0500

    Allows search static method to construct subqueries dynamically based on query parameter existence

--- models/actionFigure.ts
+++ models/actionFigure.ts
@@ -24,58 +24,41 @@ const actionFigureSchema =  new Schema<IActionFigure>(
 
 // Shape for the model inheriting Model
 interface IActionFigureModel extends Model<IActionFigure>{
-  search(searchTerm: string,variant:boolean): Promise<any>;
+  search(searchTerm: string|undefined,variant:boolean|undefined,year:string|undefined): Promise<any>;
 }
 
 // Adds static method search to model
-actionFigureSchema.statics.search = function search(searchTerm,variant){
-    return this.aggregate([
-      {
-        $search:{
-          index:"actionFigureIndex",
-          compound:{
-            filter:[
-              {
-                text:{
-                  path:"name",
-                  query:searchTerm
-                }
-              },{
-                equals:{
-                  path:"variant",
-                  value:variant
-                }
-              }
-            ]
-          }
-        }
+actionFigureSchema.statics.search = function search(name,variant,year){
+  // search stage interface
+  interface Search {
+    $search: {
+      index:string,
+      compound:{
+        must:Array<{text?:object; equals?:object}>;
       }
-    ])
+    }
+  }
+  // Search stage
+  let search :Search = {
+    $search:{
+      index:"actionFigureIndex",
+      compound:{
+        must:[]
+      }
+    }
+  }
+  // Check if a variant query parameter exists
+  if(name !=undefined){
+    search.$search.compound.must.push({text:{path:"name",query:name}})
+  }
+  if(variant!=undefined){
+    search.$search.compound.must.push({equals:{path:"variant",value:variant}})
+  }
+  if(year!=undefined){
+    search.$search.compound.must.push({text:{path:"year",query:year}})
+  }
+  return this.aggregate([search])
 }
 
 const actionFigureModel = model<IActionFigure,IActionFigureModel>('actionfigures',actionFigureSchema)
 export default actionFigureModel
-
-
-// text: {
-                //   query: searchTerm,
-                //   path: "name"
-                // }
-
-                // ([
-                //   {
-                //     $search: {
-                //       index: "actionFigureIndex",
-                //       compound:{
-                //         should:[
-                //           {
-                //             text:{
-                //             path:"name",
-                //             query:searchTerm
-                //           }
-                //         }
-                //         ]
-                //       }
-                //     }
-                //   }
-                // ])
--- routes/figure.ts
+++ routes/figure.ts
@@ -1,28 +1,39 @@
 import {Router} from 'express'
 import actionFigureModel from '../models/actionFigure'
+import { error } from 'console'
 const router = Router()
 
 // Get figure by name
 router.get('/', async(req, res) => {
-    const {name,variant} = req.query
+    const {name,variant,year} = req.query
     
-    // Validations
+    // Name
+    if (typeof name !== 'string' && typeof name !== 'undefined' ){
+        throw new Error("Name must be a single string value.")
+    }
+    // Year
+    if (typeof year !== 'string' && typeof year !== 'undefined' ){
+        throw new Error("year error")
+    }
+    // Variant
     let variantBoolean;
     if(variant==="true"){
         variantBoolean=true
     }else if(variant==="false"){
         variantBoolean=false
+    }else if(!variant){
+        variantBoolean===undefined
     }else{
-        throw new Error("Variant must be either true or false.")
+        throw new Error("Test")
     }
     
-    if (typeof name !== 'string'){
-        throw new Error("Name must be a single string value.")
-    }
+  
+
     
-   
+
+    // Query
     try{
-        const findFigure = await actionFigureModel.search(name,variantBoolean)
+        const findFigure = await actionFigureModel.search(name,variantBoolean,year)
         res.send(findFigure)
     }catch (error){
         res.status(500).json({error:'Failed'})
